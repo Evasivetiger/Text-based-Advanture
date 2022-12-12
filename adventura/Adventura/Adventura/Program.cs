df20bc2067573bf70069a38cdcfe94c42fd549f1
Adam Viszolajszky <75731714+Evasivetiger@users.noreply.github.com>
2022-12-12 09:40:16 +0100

    base

---
+++ adventura/Adventura/Adventura/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Adventura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
